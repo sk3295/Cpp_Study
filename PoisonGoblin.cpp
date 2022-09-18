2a59b47a8e254f19fbd5aea2723577b003ffb37c
sk3295 <skltepone@gmail.com>
2022-09-18 16:52:37 +0900

    PoisonGoblin

--- PoisonGoblin.cpp
+++ PoisonGoblin.cpp
@@ -6,6 +6,7 @@ void PoisonGoblin::AI() {}
 
 int  PoisonGoblin::Attack(class Character* target, float multiplier = 1.0f) {
 	int attackAmount = RandomRange(0.0f, 1.0f) < 0.7f ? 1 : 2;
+	int result = 0;
 
 	for (int i = 0; i < attackAmount; i++) {
 		result += Character::Attack(target, multiplier);
@@ -16,8 +17,27 @@ int  PoisonGoblin::Attack(class Character* target, float multiplier = 1.0f) {
 			PrintLine("에게 독이 퍼지기 시작했다.");
 		}
 	}
+
+	return result;
 }
-int  PoisonGoblin::Skill(class Character* target) {}
+int  PoisonGoblin::Skill(class Character* target) {
+	int result = 0;
+
+	if (cooldownCurrent > 0 || manaCurrent < 35) {
+		PrintLine("독 고블린의 스킬 사용에 실패했습니다.");
+	} else {
+		result = target->TakeDamage(this, poisonStack * 4);
+		poisonStack = 0;
+		Print(target->name);
+		PrintLine("에게 퍼져있던 독을 한 번에 터트린다!");
+		Print(target->name);
+		Print("에게");
+		Print(result);
+		PrintLine("의 데미지를 입었다.");
+	};
+
+	return result;
+}
 
 int  PoisonGoblin::TakeDamage(class Character* attacker, int damage) {}
 int  PoisonGoblin::Defence(class Character* target) {}
